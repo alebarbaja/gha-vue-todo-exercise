f117753adb258fa6c5df2db0105fae509545227c
Alejandro Narvaja <anarvaja@gmail.com>
2019-10-25 09:07:47 +0200

    Add code for trying to fix connection problems

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,41 @@
+/*
+ * @license
+ * Your First PWA Codelab (https://g.co/codelabs/pwa)
+ * Copyright 2019 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+'use strict';
+
+const CACHE_NAME = 'static-cache-v1';
+
 const FILES_TO_CACHE = [
     '/offline.html',
 ];
+
+self.addEventListener('install', (evt) => {
+    console.log('[ServiceWorker] Install');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+    console.log('[ServiceWorker] Activate');
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+});
 
 evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
