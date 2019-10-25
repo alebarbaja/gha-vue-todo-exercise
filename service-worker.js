31679e73c566abf1ca99bdd838411c5c63016884
Alejandro Narvaja <anarvaja@gmail.com>
2019-10-25 09:37:15 +0200

    Fix problems with connection

--- service-worker.js
+++ service-worker.js
@@ -1,71 +1,62 @@
-/*
- * @license
- * Your First PWA Codelab (https://g.co/codelabs/pwa)
- * Copyright 2019 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
- */
-'use strict';
-
-const CACHE_NAME = 'static-cache-v2';
-
-const FILES_TO_CACHE = [
+var CACHE_NAME = 'vue-todo-v1';
+var urlsToCache = [
     '/',
-    '/index.html',
     '/css/style.css',
-    '/offline.html',
+    '/js/main.js',
+    '/offline.html'
 ];
 
-self.addEventListener('install', (evt) => {
-    console.log('[ServiceWorker] Install');
-    self.skipWaiting();
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('activate', (evt) => {
-    console.log('[ServiceWorker] Activate');
-    self.clients.claim();
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if (response) {
+                    return response;
+                }
+                var fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function (response) {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function (cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                )
+            })
+    )
 });
 
-self.addEventListener('fetch', (evt) => {
-    console.log('[ServiceWorker] Fetch', evt.request.url);
-});
+self.addEventListener('activate', function (event) {
+    var cacheWhiteList = ['vue-todo-v1'];
 
-evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-        console.log('[ServiceWorker] Pre-caching offline page');
-        return cache.addAll(FILES_TO_CACHE);
-    }),
-
-    caches.keys().then((keyList) => {
-        return Promise.all(keyList.map((key) => {
-            if (key !== CACHE_NAME) {
-                console.log('[ServiceWorker] Removing old cache', key);
-                return caches.delete(key);
-            }
-        }));
-    })
-);;
-
-if (evt.request.mode !== 'navigate') {
-    return;
-}
-
-evt.respondWith(
-    fetch(evt.request)
-        .catch(() => {
-            return caches.open(CACHE_NAME)
-                .then((cache) => {
-                    return cache.match('offline.html');
-                });
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheWhiteList.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
-);
+    );
+});
